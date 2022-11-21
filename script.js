1946b3e83e90e8cfd46ca090233b3899ba4dd1c1
Joe Davitt <joseph.j.davitt@gmail.com>
2022-11-21 17:52:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // Practice JS
 
 
+//Random card generator
+
+function getCard(){
+    const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+    const idx = Math.floor(Math.random() * values.length);
+    const value = values[idx];
+
+    const suits = ['clubs','spades','hearts','diamonds'];
+    const suitsIdx = Math.floor(Math.random() * suits.length);
+    const suit = suits[suitsIdx];
+    console.log(value, suit);
+
+}
+
