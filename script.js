13bf21fabdd4b577c8301c6c7c62bd9126743257
Joe Davitt <joseph.j.davitt@gmail.com>
2022-11-22 04:17:52 +0000

    Add eightBall

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ function getCard(){
 
 }
 
+function eightBall(){
+    let userName = prompt('What is your name?');
+    userName ? console.log(`Hello, ${userName}!`)
+    : console.log('Hello!');
+    let userQuestion = prompt('What is your question?');
+    console.log(`The user asked: ${userQuestion}`);
+    let randomNumber = Math.floor(Math.random() * 8);
+    let eightBall = '';
+    console.log('Magic 8-ball says:');
+    switch (randomNumber) {
+        case 0:
+        eightBall = 'It is certain'
+        break
+        case 1:
+        eightBall = 'It is decidedly so'
+        break
+        case 2:
+        eightBall = 'Not likely'
+        break
+        case 3:
+        eightBall = 'Does the tin man have a sheet metal cock?'
+        break
+        case 4:
+        eightBall = 'I would not count on it'
+        break
+        case 5:
+        eightBall = 'My sources say yes'
+        break
+        case 6:
+        eightBall = 'Outlook uncertain'
+        break
+        case 7:
+        eightBall = 'Signs point to yes'
+        break
+    }
+    console.log(eightBall);
+}
